9fe1a53d74b826e830f5e3daf4008ad922cfce59
CupIvan <mail@cupivan.ru>
2019-11-10 17:51:08 +0300

    добавил библиотеку moment.js в CDN

--- framework/cdn.class.php
+++ framework/cdn.class.php
@@ -22,5 +22,9 @@ class cdn
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/<?=$x?>/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <?
 		}
+		if ($lib == 'moment') { $x = $version ?? '2.24.0';?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/<?=$x?>/moment.min.js"></script>
+<?
+		}
 	}
 }
